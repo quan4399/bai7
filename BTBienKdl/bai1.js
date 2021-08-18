262c83fd4f28623a550f395883fb5de928cea9a9
quan4399 <nnq4399@gmail.com>
2021-08-18 16:19:13 +0700

    bai 1

---
+++ BTBienKdl/bai1.js
@@ -0,0 +1,7 @@
+let Ly = +prompt("Diem ly");
+let Hoa = +prompt("Diem hoa");
+let Sinh = +prompt("Diem sinh");
+tb = (Ly + Hoa + Sinh) / 3;
+tong = Ly + Hoa + Sinh;
+alert("Tong: " + tong);
+alert("Diem trung binh: " + tb);
